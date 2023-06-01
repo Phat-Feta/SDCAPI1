665b08aabc955420837944602ec2abc64a7dcc73
Ubuntu <ubuntu@ip-172-31-19-225.us-east-2.compute.internal>
2023-06-01 19:47:06 +0000

    for loader.io

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const router = require('./routes.js')
 require('dotenv').config()
 
@@ -7,9 +8,14 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
+//app.use(express.static(path.join(__dirname,'../loaderio-46d9dcc7b2f9d0a7290202dabf338dd4d.txt')));
 
 app.use('/api/fec2/hr-rf', router);
+
+app.get('/loaderio-46d9dcc7b2f9d0a7290202dabf338d4d',(req,res)=>{
+	res.send('loaderio-46d9dcc7b2f9d0a7290202dabf338d4d').status(200);
+})
 
 const port = process.env.PORT || 3000;
 
